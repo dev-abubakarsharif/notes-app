e71b130806276c9f9ba11cffdc389d778aea8a70
dev-abubakarsharif <abubakarsharif.dev@gmail.com>
2026-01-16 15:14:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NoteForm from './components/NoteForm'
+const App = () => {
+  return (
+    <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shawdow-lg'> 
+    <h2 className="text-2xl font-bold mb-4 text-center">Notes App</h2>
+    <NoteForm />
+   </div>
+  )
+}
+
+export default App
